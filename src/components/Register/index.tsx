841fdebb81785fd956ab29988032bbd85a69bcc6
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2024-03-22 14:08:04 +0100

    single quote to double quote

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -67,22 +67,22 @@ export const Register = () => {
   };
 
   return (
-    <section className='flex flex-col w-full mt-10 pb-10 px-2'>
-      <h1 className='w-fill text-center py-8 text-7xl font-bold title-gradient'>
+    <section className="flex flex-col w-full mt-10 pb-10 px-2">
+      <h1 className="w-fill text-center py-8 text-7xl font-bold title-gradient">
         Regístrate
       </h1>
-      <p className='mt-6 text-[18px]'>
+      <p className="mt-6 text-[18px]">
         Aunque te hayas prescrito, debes registrarte con todos los datos que se
         solicitan en el formulario.
       </p>
-      <p className='mt-2 text-[18px]'>
+      <p className="mt-2 text-[18px]">
         Si quieres ver el resto de detalles, revisa{" "}
-        <span className='text-cGreenText underline'>el reglamento</span> antes
+        <span className="text-cGreenText underline">el reglamento</span> antes
         de enviar tu participación.
       </p>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className='flex flex-col mt-18 gap-y-7'
+        className="flex flex-col mt-18 gap-y-7"
       >
         <ProjectForm register={register} errors={errors} />
         <Participants
@@ -93,7 +93,7 @@ export const Register = () => {
         />
         <TermsAndConditions register={register} errors={errors} />
 
-        <button className='py-2 px-6 font-bold hover:bg-cGreenStroke w-fit rounded-[5px] bg-cGreenButton self-center'>
+        <button className="py-2 px-6 font-bold hover:bg-cGreenStroke w-fit rounded-[5px] bg-cGreenButton self-center">
           Enviar participación
         </button>
       </form>
