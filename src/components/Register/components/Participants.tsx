8afdf2fb8481448d29c5b3b6aad17e1fa5de82c5
Marcos S <marcosgnasa@gmail.com>
2024-03-22 10:54:09 +0100

    chore: refactor form input

--- src/components/Register/components/Participants.tsx
+++ src/components/Register/components/Participants.tsx
@@ -1,93 +1,110 @@
-import { FC } from "react";
-import { FormType } from "../../zod.schema";
-import { Input } from "./Input";
-import { Control, FieldErrors, UseFormRegister, UseFormTrigger, useFieldArray } from "react-hook-form";
-import { Plus, Trash } from "lucide-react";
+import { FC } from 'react'
+import { RegisterForm } from '../../zod.schema'
+import { Input } from './Input'
+import {
+  Control,
+  FieldErrors,
+  UseFormRegister,
+  UseFormTrigger,
+  useFieldArray,
+} from 'react-hook-form'
+import { Plus, Trash } from 'lucide-react'
 
 type Props = {
-  control: Control<FormType>;
-  register: UseFormRegister<FormType>;
-  trigger: UseFormTrigger<FormType>;
-  errors: FieldErrors<FormType>;
+  control: Control<RegisterForm>
+  register: UseFormRegister<RegisterForm>
+  trigger: UseFormTrigger<RegisterForm>
+  errors: FieldErrors<RegisterForm>['participants']
 }
 
-export const Participants: FC<Props> = ({control, register, trigger, errors}) => {
-
-  const { fields, prepend, remove } = useFieldArray({
+export const Participants: FC<Props> = ({
+  register,
+  control,
+  errors,
+  trigger,
+}) => {
+  const {
+    fields: participants,
+    prepend: addParticipant,
+    remove: removeParticipant,
+  } = useFieldArray({
     control,
-    name: "participants",
-  });
+    name: 'participants',
+  })
 
   const handleAddParticipant = () => {
     trigger('participants').then((result) => {
       if (result) {
-        prepend({ participant_country: "", participant_email: "", participant_name: "" })
+        addParticipant({
+          participant_country: '',
+          participant_email: '',
+          participant_name: '',
+        })
       }
-    });
+    })
   }
 
   return (
     <>
       <div className="mt-12 flex justify-between items-center">
-          <p className="text-5xl font-bold pb-5 title-gradient">Participantes</p>
-          <button
-            type="button"
-            onClick={handleAddParticipant}
-            className="flex justify-between py-2 h-fit px-4 font-bold hover:bg-[#444444] w-fit rounded-[5px] bg-cSecondary border border-cTertiary">
-            <Plus />
-            Añadir participante
-          </button>
-        </div>
+        <p className="text-5xl font-bold pb-5 title-gradient">Participantes</p>
+        <button
+          type="button"
+          onClick={handleAddParticipant}
+          className="flex justify-between py-2 h-fit px-4 font-bold hover:bg-[#444444] w-fit rounded-[5px] bg-cSecondary border border-cTertiary"
+        >
+          <Plus />
+          Añadir participante
+        </button>
+      </div>
 
-        {fields.map((item, index) => {
-          return (
-            <div key={item.id}>
-              {
-                0 === index ? (
-                  <div className="flex flex-col gap-y-7">
-                    <div className="grid grid-cols-2 gap-x-8">
-                      <Input
-                        label="Nombre"
-                        placeholder="Introduce tu nombre"
-                        register={register}
-                        registerName={`participants.${index}.participant_name`}
-                        error={errors.participants?.[index]?.participant_name}
-                      />
+      {participants.map((field, index) => {
+        return (
+          <div key={field.id}>
+            {0 === index ? (
+              <div className="flex flex-col gap-y-7">
+                <div className="grid grid-cols-2 gap-x-8">
+                  <Input
+                    label="Nombre*"
+                    error={errors?.[index]?.participant_name}
+                    id={`participants.${index}.participant_name`}
+                    placeholder="Introduce tu nombre"
+                    {...register(`participants.${index}.participant_name`)}
+                  />
 
-                      <Input
-                        label="País"
-                        placeholder="Introduce tu país"
-                        register={register}
-                        registerName={`participants.${index}.participant_country`}
-                        error={
-                          errors.participants?.[index]?.participant_country
-                        }
-                      />
-                    </div>
-                    <Input
-                      label="Email"
-                      placeholder="Introduce tu correo electrónico"
-                      register={register}
-                      registerName={`participants.${index}.participant_email`}
-                      error={errors.participants?.[index]?.participant_email}
-                    />
-                  </div>
-
-                ) : (
-                  <div className="flex py-6 px-5 bg-cSecondary text-cTertiary rounded-[5px] justify-between">
-                    <p>
-                      {item.participant_name} · {item.participant_country} · {item.participant_email}
-                    </p>
-                    <Trash className="hover:text-cWhite cursor-pointer" onClick={() => {
-                      remove(index)
-                    }} />
-                  </div>
-                )
-              }
-            </div>
-          )
-        })}
-    
+                  <Input
+                    label="País*"
+                    error={errors?.[index]?.participant_country}
+                    id={`participants.${index}.participant_country`}
+                    placeholder="Introduce tu país"
+                    {...register(`participants.${index}.participant_country`)}
+                  />
+                </div>
+                <Input
+                  label="Email*"
+                  error={errors?.[index]?.participant_email}
+                  id={`participants.${index}.participant_email`}
+                  placeholder="Introduce tu correo electrónico"
+                  {...register(`participants.${index}.participant_email`)}
+                />
+              </div>
+            ) : (
+              <div className="flex py-6 px-5 bg-cSecondary text-cTertiary rounded-[5px] justify-between">
+                <p>
+                  {field.participant_name} · {field.participant_country} ·{' '}
+                  {field.participant_email}
+                </p>
+                <Trash
+                  className="hover:text-cWhite cursor-pointer"
+                  onClick={() => {
+                    removeParticipant(index)
+                  }}
+                />
+              </div>
+            )}
+          </div>
+        )
+      })}
     </>
   )
 }
--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,21 +1,25 @@
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 
-import { FormType, validationSchema } from "../zod.schema";
-import { Participants } from "./components/Participants";
-import { TermsAndConditions } from "./components/TermsAndConditions";
-import { ProjectForm } from "./components/ProjectForm";
+import { RegisterForm, RegisterFormSchema } from '../zod.schema'
+import { Participants } from './components/Participants'
+import { TermsAndConditions } from './components/TermsAndConditions'
+import { ProjectForm } from './components/ProjectForm'
 
-const defaultValues = {
+const DEFAULT_REGISTER_FORM_VALUES: RegisterForm = {
+  project_name: '',
+  project_description: '',
+  project_url: '',
   participants: [
     {
-      participant_name: "",
-      participant_country: "",
-      participant_email: "",
+      participant_name: '',
+      participant_country: '',
+      participant_email: '',
     },
   ],
-};
+  terms_and_conditions: false,
+} as const
 
 export const Register = () => {
   const {
@@ -25,18 +29,16 @@ export const Register = () => {
     reset,
     control,
     trigger,
-  } = useForm<FormType>({
-    resolver: zodResolver(validationSchema),
-    defaultValues: defaultValues,
-  });
+  } = useForm<RegisterForm>({
+    resolver: zodResolver(RegisterFormSchema),
+    defaultValues: DEFAULT_REGISTER_FORM_VALUES,
+  })
 
-
-  const onSubmit = (data: FormType) => {
-    console.log("data", data);
-    reset();
-    toast.success("¡Felicidades! Acabas de registrar tu aplicación");
-  };
-
+  const onSubmit = (data: RegisterForm) => {
+    console.log('data', data)
+    reset()
+    toast.success('¡Felicidades! Acabas de registrar tu aplicación')
+  }
 
   return (
     <section className="flex flex-col w-full mt-10 pb-10 px-2">
@@ -48,16 +50,21 @@ export const Register = () => {
         solicitan en el formulario.
       </p>
       <p className="mt-2 text-[18px]">
-        Si quieres ver el resto de detalles, revisa{" "}
-        <span className="text-cGreenText underline">el reglamento</span> antes de enviar tu
-        participación.
+        Si quieres ver el resto de detalles, revisa{' '}
+        <span className="text-cGreenText underline">el reglamento</span> antes
+        de enviar tu participación.
       </p>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col mt-18 gap-y-7"
       >
         <ProjectForm register={register} errors={errors} />
-        <Participants control={control} errors={errors} trigger={trigger} register={register} />
+        <Participants
+          register={register}
+          control={control}
+          errors={errors.participants}
+          trigger={trigger}
+        />
         <TermsAndConditions register={register} errors={errors} />
 
         <button className="py-2 px-6 font-bold hover:bg-cGreenStroke w-fit rounded-[5px] bg-cGreenButton self-center">
@@ -65,5 +72,5 @@ export const Register = () => {
         </button>
       </form>
     </section>
-  );
-};
+  )
+}
