98bf97c0facb0473c8b4266b730234aaa056dc28
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-23 00:39:38 -0300

    feat(hero-section): 🌟 transform Header to Hero with enhanced responsive design
    - Rename `Header` component to `Hero` for better semantic clarity.
    - Switch wrapper element from `<header>` to `<section>` and `<article>` for text content, aligning with semantic HTML practices.
    - Introduce `hero-title`, `hero-description`, and `hero-caption` classes for targeted styling.
    - Implement responsive design enhancements with absolute positioning for the Spline component in smaller viewports.
    - Adjust text scaling and spacing to improve readability and visual impact across devices.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import Spline from '@splinetool/react-spline'
+import { ButtonLink } from './common/ButtonLink'
+
+export const Hero = () => {
+  return (
+    <section id="hero" className="flex justify-between mt-20 relative">
+      <article>
+        <h1 className="hero-title max-w-[12ch] title-gradient font-bold md:leading-28">
+          La Hackathon Del Dev
+        </h1>
+        <h2 className="mt-10 text-[#AEB2B9] font-semibold hero-description">
+          Una hackathon para cualquier dev
+        </h2>
+        <p className="text-[#AEB2B9] font-medium hero-caption mb-[24px]">
+          Próxima edición 13 de abril del 2024
+        </p>
+        <ButtonLink to="register">Registrarme</ButtonLink>
+      </article>
+      <div className="h-[600px] w-fit max-lg:absolute max-lg:inset-y-0 max-lg:right-0 my-auto max-lg:-z-1">
+        <Spline scene="https://prod.spline.design/M1HgjURyO4sZWucZ/scene.splinecode" />
+      </div>
+    </section>
+  )
+}
