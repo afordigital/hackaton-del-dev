852bcbc1539b79e95605cca8dda42c0f60c529ff
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-23 01:38:48 -0300

    feat(navigation): 📱 enhance Nav with responsive design and mobile toggle
    - Integrate `useState` hook for managing the mobile navigation menu state, adding dynamic interaction for mobile users.
    - Implement a button with the `AlignJustify` icon from `lucide-react` to toggle the navigation menu on smaller screens, improving accessibility and user experience.
    - Utilize the `cn` utility function for conditional and dynamic className management, streamlining the process of applying responsive and state-dependent styles.
    - Refactor the navigation structure to be wrapped in a `<header>` for semantic correctness and better layout control in the home route context.
    - Adjust the navigation styling to accommodate varying screen sizes with conditional transformation classes, ensuring a cohesive look and feel across devices.
    - Simplify and enhance the return statement logic to accommodate both mobile and desktop layouts within a single conditional structure, optimizing code readability and maintenance.
    - Retain the functionality for scrolling to specific sections of the page upon link activation, preserving user-friendly navigation features.
    
    This update significantly improves the navigation component's responsiveness and interactivity, catering to a broader range of devices and user preferences.
    
    chore: improve spacing
    
    refactor(nav): 💄 add justify center for the navbar

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
 export const Footer = () => {
   return (
-    <footer className="w-full text-center text-[24px] text-cwhite mb-8 text-[#A8A8A8] font-bold">
+    <footer className="w-full text-center text-[24px] py-20 text-cwhite mb-8 text-[#A8A8A8] font-bold">
       Diseñado y desarrollado con 💚 por{' '}
-      <a href="https://www.twitch.tv/afor_digital" className="hover:underline">afor_digital</a>
+      <a href="https://www.twitch.tv/afor_digital" className="hover:underline">
+        afor_digital
+      </a>
     </footer>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ import { Sponsors } from './Sponsors'
 
 export const Home = () => {
   return (
-    <div className="flex flex-col gap-y-[80px] container mx-auto">
+    <div className="flex flex-col gap-y-[80px] container mx-auto mt-10">
       <Hero />
       <Rules />
       <Sponsors />
