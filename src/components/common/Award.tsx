2f815429c31f0383bf3eb8f10f2857dda3a4ed31
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-23 00:05:15 -0300

    refactor(components): 🎨 improve Award component structure and styling
    - Standardize import quotes to use single quotes.
    - Add `w-full` class to ensure Award component takes full width.
    - Adjust `img` element class to use `my-auto` for vertical alignment.
    - Remove commented-out code for cleaner component definition.

--- src/components/common/Award.tsx
+++ src/components/common/Award.tsx
@@ -1,17 +1,24 @@
-import { AWARD_TYPE } from "../../constants";
+import { AWARD_TYPE } from '../../constants'
 
 type AwardProps = {
-    img: string;
-    alt: string;
-    title: string;
-    awardType: AWARD_TYPE
+  img: string
+  alt: string
+  title: string
+  awardType: AWARD_TYPE
 }
 
-export const Award = ({img, alt, title, awardType}: AwardProps) => {
+export const Award = ({ img, alt, title, awardType }: AwardProps) => {
   return (
-    <div className={`box bg-cSecondary h-full drop-shadow-customShadow ${awardType===AWARD_TYPE.PRICE ? 'px-20' : 'px-8' } flex flex-col gap-4 items-center border border-cStrokeBox rounded-[5px] py-8`}>
-        <img src={`/images/rewards/${img}`} alt={alt} className="object-contain h-[200px]" />
-        <p className={`text-cWhite text-center ${awardType===AWARD_TYPE.PRICE ? 'text-[64px]' : 'text-[24px]'}  font-bold`}>{title}</p>
+    <div
+      className={`box bg-cSecondary h-full drop-shadow-customShadow ${awardType === AWARD_TYPE.PRICE ? 'px-20' : 'px-8'} flex flex-col gap-4 items-center border border-cStrokeBox rounded-[5px] py-8 w-full`}
+    >
+      <img src={`/images/rewards/${img}`} alt={alt} className="my-auto" />
+
+      <p
+        className={`text-cWhite text-center ${awardType === AWARD_TYPE.PRICE ? 'text-[64px]' : 'text-[24px]'}  font-bold`}
+      >
+        {title}
+      </p>
     </div>
   )
 }
