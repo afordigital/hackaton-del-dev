e8cc54c75c545ac5e5723fb39d64052a0997a1bc
Samuel Llibre Santos <kuzenshi@gmail.com>
2024-03-23 02:14:51 -0300

    style: 🔥 clean up

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -9,9 +9,7 @@ export const Sponsors = () => {
       <Title>Patrocinadores</Title>
       <section className="grid md:grid-cols-[1fr_0.5fr_1fr] place-items-center gap-32 w-full">
         <img src="/images/sponsors/logitech.svg" alt="logitech logo" />
-
         <img src="/images/sponsors/getmagical.svg" alt="getmagical logo" />
-
         <img src="/images/sponsors/4geeks.svg" alt="4geeks logo" />
       </section>
     </article>
--- src/components/common/Rule.tsx
+++ src/components/common/Rule.tsx
@@ -7,12 +7,7 @@ type RuleProps = {
 export const Rule = ({ id, alt, description }: RuleProps) => {
   return (
     <div className="grid place-items-center gap-2">
-      <img
-        loading="lazy"
-        src={`/images/rules/rule_${id}.png`}
-        alt={alt}
-        className=""
-      />
+      <img loading="lazy" src={`/images/rules/rule_${id}.png`} alt={alt} />
       <p className="text-center font-bold text-[24px] text-balance">
         {description}
       </p>
