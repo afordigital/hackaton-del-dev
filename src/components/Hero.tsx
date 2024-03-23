91054410cf93783b0fc4312a4c29a07050986b76
LUISEDOCCOR <luiseduardoocegueda@gmail.com>
2024-03-23 14:14:37 -0600

    Modify responsive design in Hero.tsx component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { ButtonLink } from './common/ButtonLink'
 
 export const Hero = () => {
   return (
-    <section id="hero" className="flex justify-between mt-20 relative">
+    <section id="hero" className="flex justify-between relative md:mt-20">
       <article className="relative z-10">
         <h1 className="hero-title max-w-[12ch] title-gradient font-bold md:leading-28">
           La Hackathon Del Dev
@@ -16,7 +16,7 @@ export const Hero = () => {
         </p>
         <ButtonLink to="register">Registrarme</ButtonLink>
       </article>
-      <div className="h-[600px] w-fit max-lg:absolute max-lg:inset-y-0 max-lg:right-0 my-auto max-lg:z-0">
+      <div className="h-[600px] w-fit max-lg:absolute max-lg:inset-y-0 max-lg:right-0 my-auto max-lg:z-0 hidden md:block">
         <Spline scene="https://prod.spline.design/M1HgjURyO4sZWucZ/scene.splinecode" />
       </div>
     </section>
